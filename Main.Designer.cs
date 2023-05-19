54788ce3cc4e9640a30195d53bcf891055e5f511
AtrinNoori <atrin_xbox@yahoo.com>
2023-05-19 17:53:23 +0330

    Just some Ui changes

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -42,7 +42,7 @@ namespace TextEncryptorAndDecryptor
             // KeyINFO
             // 
             this.KeyINFO.AutoSize = true;
-            this.KeyINFO.Location = new System.Drawing.Point(23, 17);
+            this.KeyINFO.Location = new System.Drawing.Point(23, 18);
             this.KeyINFO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.KeyINFO.Name = "KeyINFO";
             this.KeyINFO.Size = new System.Drawing.Size(31, 16);
@@ -52,7 +52,7 @@ namespace TextEncryptorAndDecryptor
             // TextINFO
             // 
             this.TextINFO.AutoSize = true;
-            this.TextINFO.Location = new System.Drawing.Point(23, 71);
+            this.TextINFO.Location = new System.Drawing.Point(23, 69);
             this.TextINFO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TextINFO.Name = "TextINFO";
             this.TextINFO.Size = new System.Drawing.Size(34, 16);
@@ -62,7 +62,7 @@ namespace TextEncryptorAndDecryptor
             // ResultINFO
             // 
             this.ResultINFO.AutoSize = true;
-            this.ResultINFO.Location = new System.Drawing.Point(276, 17);
+            this.ResultINFO.Location = new System.Drawing.Point(256, 18);
             this.ResultINFO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ResultINFO.Name = "ResultINFO";
             this.ResultINFO.Size = new System.Drawing.Size(49, 16);
@@ -87,7 +87,7 @@ namespace TextEncryptorAndDecryptor
             // 
             // Result_TXT
             // 
-            this.Result_TXT.Location = new System.Drawing.Point(273, 39);
+            this.Result_TXT.Location = new System.Drawing.Point(253, 39);
             this.Result_TXT.Multiline = true;
             this.Result_TXT.Name = "Result_TXT";
             this.Result_TXT.Size = new System.Drawing.Size(213, 131);
@@ -108,7 +108,7 @@ namespace TextEncryptorAndDecryptor
             // 
             this.Start_BTN.Location = new System.Drawing.Point(19, 184);
             this.Start_BTN.Name = "Start_BTN";
-            this.Start_BTN.Size = new System.Drawing.Size(106, 42);
+            this.Start_BTN.Size = new System.Drawing.Size(140, 42);
             this.Start_BTN.TabIndex = 7;
             this.Start_BTN.Text = "Start";
             this.Start_BTN.UseVisualStyleBackColor = true;
@@ -118,7 +118,7 @@ namespace TextEncryptorAndDecryptor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 241);
+            this.ClientSize = new System.Drawing.Size(486, 241);
             this.Controls.Add(this.Start_BTN);
             this.Controls.Add(this.Flag_CHBX);
             this.Controls.Add(this.Result_TXT);
@@ -128,7 +128,7 @@ namespace TextEncryptorAndDecryptor
             this.Controls.Add(this.TextINFO);
             this.Controls.Add(this.KeyINFO);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main";
